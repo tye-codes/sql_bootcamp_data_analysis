c88a8299e8d1d3c382b8bd13032117978a6d6b74
tye-codes <tye.routh@gmail.com>
2026-05-21 13:14:57 +1000

    Add SQL queries for date exploration analysis

---
+++ date_exploration.sql
@@ -0,0 +1,21 @@
+/* 
+SELECT DISTINCT is useful for giving a high-level overview of the dimensions available in the data.
+*/
+
+/*
+DATEDIFF is a useful tool for finding the difference between dates during exploratory data analysis. 
+*/
+
+-- Find the date of the first and last order and how many years of sales available.
+
+SELECT 
+	MIN(order_date) first_order_date,
+	MAX(order_date) last_order_date,
+	DATEDIFF(year, MIN(order_date), MAX(order_date)) order_range_years
+FROM gold.fact_sales
+
+-- Find youngest and oldest customers in the data.
+SELECT
+	DATEDIFF(YEAR, MIN(birthdate), GETDATE()) oldest_age,
+	DATEDIFF(YEAR, MAX(birthdate), GETDATE()) youngest_age
+FROM gold.dim_customers
